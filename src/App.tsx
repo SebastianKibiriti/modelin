fcfbdc990186c24ee15f61f63e85d9a010cc0aeb
Seb <111571307+SebastianKibiriti@users.noreply.github.com>
2025-07-18 11:42:12 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import ProductShowcase from './components/ProductShowcase';
-import Difference from './components/Difference';
+import Igniting from './components/Igniting';
 import UseCase from './components/UseCase';
 import Feedback from './components/Feedback';
 import CTA from './components/CTA';
